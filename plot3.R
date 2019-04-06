1b7e0da7057a29c2481574667efc83fd4f7b798f
LadySoulnight <SzybiakMartina@gmail.com>
2019-04-06 08:49:45 +0200

    Solution to course project #1

---
+++ plot3.R
@@ -0,0 +1,42 @@
+Sys.setlocale(category = "LC_ALL", locale = "english")
+
+#Read the file in
+household_power_consumption <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", header = TRUE, sep = ";", colClasses = "character")
+
+#Clean up the file, so only having the data, that is needed
+data <- data.frame(
+  subset(
+    household_power_consumption,
+    household_power_consumption$Date %in% c("1/2/2007", "2/2/2007") & household_power_consumption$Global_active_power != "?",
+    c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+  )
+)
+
+# Convert all columns as they are needed
+data <- data.frame(
+  Global_active_power = as.numeric(data$Global_active_power),
+  Global_reactive_power = as.numeric(data$Global_reactive_power),
+  Voltage = as.numeric(data$Voltage),
+  Sub_metering_1 = as.numeric(data$Sub_metering_1),
+  Sub_metering_2 = as.numeric(data$Sub_metering_2),
+  Sub_metering_3 = as.numeric(data$Sub_metering_3),
+  Datetime = strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+)
+
+#Open PNG device
+png(filename = "plot3.png", width = 480, height = 480)
+
+#Plot for Plot3
+plot(
+  data$Datetime, data$Sub_metering_1,
+  type = "l",
+  xlab = "",
+  ylab = "Energy sub metering"
+)
+# Adding additional lines into the plot
+lines(data$Datetime, data$Sub_metering_2, col = "red")
+lines(data$Datetime, data$Sub_metering_3, col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black","red", "blue"), lty = 1)
+
+# Close device
+dev.off()
